4e8cd34294b9ac09c1c881cb32eadcf0ea092ba3
mldesignerjs <mldesignerjs@gmail.com>
2024-01-07 23:25:48 +0700

    change-css

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,17 @@
 'use client'
 import { useState } from 'react'
-import { faCircleCheck, faCircleXmark, faPenToSquare } from '@fortawesome/free-solid-svg-icons'
-import { engraveFonts, kindHandle, umbrellas, handleO, umbrellaO } from '@/constants'
+import {
+    faCircleCheck,
+    faCircleXmark,
+    faPenToSquare,
+} from '@fortawesome/free-solid-svg-icons'
+import {
+    engraveFonts,
+    kindHandle,
+    umbrellas,
+    handleO,
+    umbrellaO,
+} from '@/constants'
 
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { EditContent } from './EditContent'
@@ -9,7 +19,9 @@ import { EditContent } from './EditContent'
 export default function Home() {
     const [lineNumber, setLineNumber] = useState<string>('1')
     const [lineHeight, setLineHeight] = useState<string>('0')
-    const [currentUmbrella, setCurrentUmbrella] = useState<umbrellaO>(umbrellas[0])
+    const [currentUmbrella, setCurrentUmbrella] = useState<umbrellaO>(
+        umbrellas[0],
+    )
     const [handle, setHandle] = useState<handleO>(umbrellas[0].handle[0])
     const [showEditMode, setShowEditMode] = useState<boolean>(true)
 
@@ -55,13 +67,17 @@ export default function Home() {
 
     function handleChangeUmbrella(value: string) {
         const index: number = Number(value)
-        let nextUmbrella: any = umbrellas.find((umbrella) => umbrella.id === index)
+        let nextUmbrella: any = umbrellas.find(
+            (umbrella) => umbrella.id === index,
+        )
         setCurrentUmbrella(nextUmbrella)
         setHandle(nextUmbrella.handle[0])
     }
 
     function handleChangeKindWood(value: string) {
-        const nextHandle: any = currentUmbrella?.handle.find((umbrella) => umbrella.value === value)
+        const nextHandle: any = currentUmbrella?.handle.find(
+            (umbrella) => umbrella.value === value,
+        )
         setHandle(nextHandle)
     }
 
@@ -79,9 +95,15 @@ export default function Home() {
                 onClick={handleShowEditMode}
             >
                 {showEditMode ? (
-                    <FontAwesomeIcon className="text-red-400" icon={faCircleXmark} />
+                    <FontAwesomeIcon
+                        className="text-red-400"
+                        icon={faCircleXmark}
+                    />
                 ) : (
-                    <FontAwesomeIcon className="text-blue-400" icon={faPenToSquare} />
+                    <FontAwesomeIcon
+                        className="text-blue-400"
+                        icon={faPenToSquare}
+                    />
                 )}
             </button>
             <div
@@ -91,7 +113,10 @@ export default function Home() {
                 <div className="p-4">
                     <p className="pr-6">Số dòng chữ:</p>
                     <div className="flex items-center my-4">
-                        <label className="cursor-pointer mr-6" htmlFor="lineNumber1">
+                        <label
+                            className="cursor-pointer mr-6"
+                            htmlFor="lineNumber1"
+                        >
                             <input
                                 type="radio"
                                 className="peer sr-only"
@@ -99,13 +124,20 @@ export default function Home() {
                                 id="lineNumber1"
                                 value="1"
                                 checked={lineNumber === '1'}
-                                onChange={(e) => handleChangeLineNumber(e.target.value)}
+                                onChange={(e) =>
+                                    handleChangeLineNumber(e.target.value)
+                                }
                             />
                             <div className="w-32 max-w-xl rounded-md bg-white p-2 text-gray-300 ring-2 ring-transparent transition-all hover:shadow peer-checked:text-blue-600 peer-checked:ring-blue-400 peer-checked:ring-offset-0">
                                 <div className="flex flex-col gap-1">
                                     <div className="flex items-center justify-between">
-                                        <p className="text-sm font-semibold uppercase text-gray-500">1 dòng</p>
-                                        <FontAwesomeIcon icon={faCircleCheck} className="text-xl" />
+                                        <p className="text-sm font-semibold uppercase text-gray-500">
+                                            1 dòng
+                                        </p>
+                                        <FontAwesomeIcon
+                                            icon={faCircleCheck}
+                                            className="text-xl"
+                                        />
                                     </div>
                                 </div>
                             </div>
@@ -118,13 +150,20 @@ export default function Home() {
                                 id="lineNumber2"
                                 value="2"
                                 checked={lineNumber === '2'}
-                                onChange={(e) => handleChangeLineNumber(e.target.value)}
+                                onChange={(e) =>
+                                    handleChangeLineNumber(e.target.value)
+                                }
                             />
                             <div className="w-32 max-w-xl rounded-md bg-white p-2 text-gray-300 ring-2 ring-transparent transition-all hover:shadow peer-checked:text-blue-600 peer-checked:ring-blue-400 peer-checked:ring-offset-0">
                                 <div className="flex flex-col gap-1">
                                     <div className="flex items-center justify-between">
-                                        <p className="text-sm font-semibold uppercase text-gray-500">2 dòng</p>
-                                        <FontAwesomeIcon icon={faCircleCheck} className="text-xl" />
+                                        <p className="text-sm font-semibold uppercase text-gray-500">
+                                            2 dòng
+                                        </p>
+                                        <FontAwesomeIcon
+                                            icon={faCircleCheck}
+                                            className="text-xl"
+                                        />
                                     </div>
                                 </div>
                             </div>
@@ -136,7 +175,9 @@ export default function Home() {
                             id="umbrella"
                             name="umbrella"
                             value={currentUmbrella.id}
-                            onChange={(e) => handleChangeUmbrella(e.target.value)}
+                            onChange={(e) =>
+                                handleChangeUmbrella(e.target.value)
+                            }
                         >
                             {umbrellas.map((umbrella) => (
                                 <option key={umbrella.id} value={umbrella.id}>
@@ -149,17 +190,26 @@ export default function Home() {
                             id="handle"
                             name="handle"
                             value={handle?.value}
-                            onChange={(e) => handleChangeKindWood(e.target.value)}
+                            onChange={(e) =>
+                                handleChangeKindWood(e.target.value)
+                            }
                         >
                             {currentUmbrella.handle.map((handle) => (
                                 <option key={handle.id} value={handle.value}>
-                                    {kindHandle.find((wood) => wood.value === handle.value)?.name}
+                                    {
+                                        kindHandle.find(
+                                            (wood) =>
+                                                wood.value === handle.value,
+                                        )?.name
+                                    }
                                 </option>
                             ))}
                         </select>
                     </div>
                     <div className="border-t-gray-300 py-4 border-t">
-                        {lineNumber === '2' && <p className="font-bold text-center">Dòng 1</p>}
+                        {lineNumber === '2' && (
+                            <p className="font-bold text-center">Dòng 1</p>
+                        )}
                         <EditContent
                             text={text1}
                             fontSize={fontSize1}
@@ -179,12 +229,17 @@ export default function Home() {
                                     letterSpacing={letterSpacing2}
                                     onChangeContent={handleChangeContent2}
                                     onChangeFontSize={handleChangeFontSize2}
-                                    onChangeLetterSpacing={handleChangeLetterSpacing2}
+                                    onChangeLetterSpacing={
+                                        handleChangeLetterSpacing2
+                                    }
                                 />
                             </div>
                             <div className="border-t-gray-300 border-t">
                                 <div className="py-2 relative">
-                                    <label className="pr-6 block" htmlFor="letterSpacing">
+                                    <label
+                                        className="pr-6 block"
+                                        htmlFor="letterSpacing"
+                                    >
                                         Khoảng cách 2 dòng:
                                     </label>
                                     <span className="font-bold text-main absolute left-1/2 -translate-x-1/2">
@@ -198,7 +253,11 @@ export default function Home() {
                                         value={lineHeight}
                                         min={-20}
                                         max={20}
-                                        onChange={(e) => handleChangeLineHeight(e.target.value)}
+                                        onChange={(e) =>
+                                            handleChangeLineHeight(
+                                                e.target.value,
+                                            )
+                                        }
                                     />
                                 </div>
                             </div>
@@ -208,7 +267,11 @@ export default function Home() {
             </div>
             <div className="mt-6">
                 {engraveFonts.map((font, key) => (
-                    <div key={key} style={font.style} className="w-full overflow-hidden justify-center pb-6 relative">
+                    <div
+                        key={key}
+                        style={font.style}
+                        className="w-full overflow-hidden justify-center pb-6 relative"
+                    >
                         <span className="max-sm:text-sm text-xl absolute max-sm:top-2 max-sm:left-2 top-4 left-4 z-10 flex max-sm:w-5 max-sm:h-5 w-10 h-10 rounded-full bg-main text-white justify-center items-center">
                             {key + 1}
                         </span>
@@ -217,9 +280,9 @@ export default function Home() {
                                 <img
                                     src={handle.urlImg}
                                     alt=""
-                                    className="max-sm:h-[35mm] md:h-[52.5mm] lg:h-[7cm] w-max min-w-max"
+                                    className="w-max min-w-max handle-image"
                                 />
-                                <div className="flex flex-col justify-center absolute top-1/2 -translate-y-1/2 origin-left max-sm:scale-50 max-lg:scale-75 max-sm:left-[15mm] max-lg:left-[22.5mm] lg:left-[3cm] whitespace-nowrap overflow-hidden text-center h-[15mm]">
+                                <div className="content">
                                     <span
                                         className={`engrave leading-none block ${handle.value}`}
                                         style={{
